d58610f2b227d688288627e0beb060b60844c8aa
Kamil Brucki <brucek12@interia.pl>
2026-08-26 07:52:47 +0200

    CreateFolioItemCommandHandler tests

--- Hotel.Application/FiscalAccounting/Commands/CreateFolioItemCommandHandler.cs
+++ Hotel.Application/FiscalAccounting/Commands/CreateFolioItemCommandHandler.cs
@@ -1,7 +1,6 @@
 using Hotel.Application.Configurations.Services;
 using Hotel.Domain.FiscalAccounting.Enums;
 using Hotel.Domain.Transactions.Enums;
-using Hotel.Domain.Transactions.Services;
 using MediatR;
 using Hotel.Domain.Transactions.Repositories;
 using Hotel.Domain.FiscalAccounting.Repositories;
