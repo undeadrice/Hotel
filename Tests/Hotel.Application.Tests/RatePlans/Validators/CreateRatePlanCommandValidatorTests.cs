3033350716490d9e12c09ab53d85e6105cf104c1
Kamil Brucki <brucek12@interia.pl>
2026-08-28 19:30:29 +0200

    CreateRatePlanCommandValidator and tests

---
+++ Tests/Hotel.Application.Tests/RatePlans/Validators/CreateRatePlanCommandValidatorTests.cs
@@ -0,0 +1,130 @@
+using FluentValidation.TestHelper;
+using Hotel.Application.RatePlans.Commands;
+using Hotel.Application.RatePlans.Validators;
+using Xunit;
+
+namespace Hotel.Application.Tests.RatePlans.Validators;
+
+public class CreateRatePlanCommandValidatorTests
+{
+    private readonly CreateRatePlanCommandValidator _validator = new();
+
+    private static CreateRatePlanCommand CreateValidCommand() =>
+        new(
+            "Peak Season",
+            Guid.NewGuid(),
+            new DateOnly(2026, 8, 10),
+            new DateOnly(2026, 8, 12),
+            new List<CreateRatePlanRoomCommand> { new(Guid.NewGuid(), 100m) });
+
+    [Fact]
+    public void Validate_WithValidCommand_ShouldNotHaveValidationErrors()
+    {
+        // Arrange
+        var command = CreateValidCommand();
+
+        // Act
+        var result = _validator.TestValidate(command);
+
+        // Assert
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [Fact]
+    public void Validate_WithEmptyName_ShouldHaveValidationError()
+    {
+        // Arrange
+        var command = CreateValidCommand() with { Name = "" };
+
+        // Act
+        var result = _validator.TestValidate(command);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Name);
+    }
+
+    [Fact]
+    public void Validate_WithEmptyTransactionCodeId_ShouldHaveValidationError()
+    {
+        // Arrange
+        var command = CreateValidCommand() with { TransactionCodeId = Guid.Empty };
+
+        // Act
+        var result = _validator.TestValidate(command);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.TransactionCodeId);
+    }
+
+    [Fact]
+    public void Validate_WithDefaultStartDate_ShouldHaveValidationError()
+    {
+        // Arrange
+        var command = CreateValidCommand() with { StartDate = default };
+
+        // Act
+        var result = _validator.TestValidate(command);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.StartDate);
+    }
+
+    [Fact]
+    public void Validate_WithDefaultEndDate_ShouldHaveValidationError()
+    {
+        // Arrange
+        var command = CreateValidCommand() with { EndDate = default };
+
+        // Act
+        var result = _validator.TestValidate(command);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.EndDate);
+    }
+
+    [Fact]
+    public void Validate_WhenStartDateIsNotBeforeEndDate_ShouldHaveValidationError()
+    {
+        // Arrange
+        var command = CreateValidCommand() with
+        {
+            StartDate = new DateOnly(2026, 8, 12),
+            EndDate = new DateOnly(2026, 8, 10)
+        };
+
+        // Act
+        var result = _validator.TestValidate(command);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.StartDate);
+    }
+
+    [Fact]
+    public void Validate_WithEmptyRooms_ShouldHaveValidationError()
+    {
+        // Arrange
+        var command = CreateValidCommand() with { Rooms = new List<CreateRatePlanRoomCommand>() };
+
+        // Act
+        var result = _validator.TestValidate(command);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Rooms);
+    }
+
+    [Fact]
+    public void Validate_WithRoomWithEmptyRoomTypeId_ShouldHaveValidationError()
+    {
+        // Arrange
+        var command = CreateValidCommand() with
+        {
+            Rooms = new List<CreateRatePlanRoomCommand> { new(Guid.Empty, 100m) }
+        };
+
+        // Act
+        var result = _validator.TestValidate(command);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor("Rooms[0].RoomTypeId");
+    }
+}
