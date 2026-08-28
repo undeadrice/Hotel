b424ec8fd9dbdea3ba02f218235b9ea07e04c120
Kamil Brucki <brucek12@interia.pl>
2026-08-28 18:26:59 +0200

    UpdateRoomCommandValidator and tests

---
+++ Hotel.Application/Rooming/Validators/UpdateRoomCommandValidator.cs
@@ -0,0 +1,20 @@
+using FluentValidation;
+using Hotel.Application.Rooming.Commands;
+
+namespace Hotel.Application.Rooming.Validators;
+
+public class UpdateRoomCommandValidator : AbstractValidator<UpdateRoomCommand>
+{
+    public UpdateRoomCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Room id is required.");
+
+        RuleFor(x => x.RoomNumber)
+            .NotEmpty().WithMessage("Room number is required.")
+            .MaximumLength(20).WithMessage("Room number must not exceed 20 characters.");
+
+        RuleFor(x => x.RoomTypeId)
+            .NotEmpty().WithMessage("Room type is required.");
+    }
+}
