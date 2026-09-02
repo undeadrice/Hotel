5e65f39493f1f5235bee61663e94623cdc83f96b
Kamil Brucki <brucek12@interia.pl>
2026-09-02 19:01:29 +0200

    Fixed formatting

--- Hotel.Application/Roles/Queries/GetRoleListQueryHandler.cs
+++ Hotel.Application/Roles/Queries/GetRoleListQueryHandler.cs
@@ -4,30 +4,15 @@ using Hotel.Application.Roles.Services;
 using Hotel.Application.Users.Enums;
 using MediatR;
 
-
-
 namespace Hotel.Application.Roles.Queries;
 
-
-
 [CheckPermission(Permission.RoleView)]
-
 public record GetRoleListQuery() : IRequest<IReadOnlyCollection<RoleSimpleDto>>;
-
-
-
 internal class GetRoleListQueryHandler(IRoleService roleService)
-
     : IRequestHandler<GetRoleListQuery, IReadOnlyCollection<RoleSimpleDto>>
-
 {
-
     public async Task<IReadOnlyCollection<RoleSimpleDto>> Handle(GetRoleListQuery request, CancellationToken cancellationToken)
-
     {
-
         return await roleService.GetAll();
-
     }
-
 }
