0df0275239ef75eb70af6c5d7876451d92ab094f
Kamil Brucki <brucek12@interia.pl>
2026-09-04 16:01:24 +0200

    Fixed formatting

--- Hotel.Application/Roles/Commands/UpdateRoleCommandHandler.cs
+++ Hotel.Application/Roles/Commands/UpdateRoleCommandHandler.cs
@@ -3,28 +3,15 @@ using Hotel.Application.Roles.Services;
 using Hotel.Application.Users.Enums;
 using MediatR;
 
-
-
-namespace Hotel.Application.Roles.Commands;
-
-
+namespace Hotel.Application.Roles.Commands
 
 [CheckPermission(Permission.RoleEdit)]
-
 public record UpdateRoleCommand(Guid Id, string Name, IReadOnlyCollection<string> Permissions) : ICommand;
 
-
-
 public class UpdateRoleCommandHandler(IRoleService roleService) : IRequestHandler<UpdateRoleCommand>
-
 {
-
     public async Task Handle(UpdateRoleCommand request, CancellationToken cancellationToken)
-
     {
-
         await roleService.Update(request.Id, request.Name, request.Permissions);
-
     }
-
 }
