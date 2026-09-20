d73b6888b92eaad7cadde15c16878d78c9accc3d
Kamil Brucki <brucek12@interia.pl>
2026-09-20 10:04:22 +0200

    Guest tests now check exception names

--- Tests/Hotel.IntegrationTests/Guests/Commands/UpdateGuestCommandTests.cs
+++ Tests/Hotel.IntegrationTests/Guests/Commands/UpdateGuestCommandTests.cs
@@ -3,6 +3,8 @@ using Hotel.Application.Guests.Commands;
 using Hotel.Application.Guests.TransferObjects;
 using Hotel.IntegrationTests.Infrastructure;
 using Hotel.IntegrationTests.Infrastructure.TestData;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System.Net;
 using System.Net.Http.Json;
 using Xunit;
@@ -65,5 +67,10 @@ public class UpdateGuestCommandTests : IClassFixture<HotelWebApplicationFactory>
 
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+
+        var problemDetails = await response.Content.ReadFromJsonAsync<ProblemDetails>();
+
+        problemDetails!.Status.Should().Be(StatusCodes.Status404NotFound);
+        problemDetails.Extensions["exception"]!.ToString().Should().Be("NotFoundException");
     }
 }
--- Tests/Hotel.IntegrationTests/Guests/Queries/GetGuestByIdQueryTests.cs
+++ Tests/Hotel.IntegrationTests/Guests/Queries/GetGuestByIdQueryTests.cs
@@ -2,6 +2,8 @@ using FluentAssertions;
 using Hotel.Application.Guests.TransferObjects;
 using Hotel.IntegrationTests.Infrastructure;
 using Hotel.IntegrationTests.Infrastructure.TestData;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System.Net;
 using System.Net.Http.Json;
 using Xunit;
@@ -66,5 +68,10 @@ public class GetGuestByIdQueryTests : IClassFixture<HotelWebApplicationFactory>,
 
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+
+        var problemDetails = await response.Content.ReadFromJsonAsync<ProblemDetails>();
+
+        problemDetails!.Status.Should().Be(StatusCodes.Status404NotFound);
+        problemDetails.Extensions["exception"]!.ToString().Should().Be("NotFoundException");
     }
 }
