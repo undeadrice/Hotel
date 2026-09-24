7b5d59f939e3cf2d45adc8a2e4f126d1aa60e4fe
Kamil Brucki <brucek12@interia.pl>
2026-09-24 17:33:14 +0200

    Registrations

--- AWS/Lambdas/Auth/Hotel.Auth.Application/DIRegistrations.cs
+++ AWS/Lambdas/Auth/Hotel.Auth.Application/DIRegistrations.cs
@@ -1,3 +1,5 @@
+using FluentValidation;
+using Hotel.Auth.Application.Seeding;
 using Hotel.SharedPipeline.Behaviors;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,10 +11,13 @@ public static class DIRegistrations
     public static IServiceCollection AddAuthApplication(this IServiceCollection services)
     {
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(DIRegistrations).Assembly));
+        services.AddValidatorsFromAssembly(typeof(DIRegistrations).Assembly);
 
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CheckRoleBehavior<,>));
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CheckPermissionBehavior<,>));
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+        services.AddScoped<ISeedingService, SeedingService>();
 
         return services;
     }
