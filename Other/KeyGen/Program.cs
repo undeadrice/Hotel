a7b62ca2a7f5c50ff2fed1d3c198ecae80fc495b
Kamil Brucki <brucek12@interia.pl>
2026-09-26 10:41:18 +0200

    Added keygen

---
+++ Other/KeyGen/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Cryptography;
+
+var folder = @"C:\Users\bruce\Desktop\ssl";
+Directory.CreateDirectory(folder);
+
+using var rsa = RSA.Create(2048);
+File.WriteAllText(Path.Combine(folder, "private.pem"), rsa.ExportRSAPrivateKeyPem());
+File.WriteAllText(Path.Combine(folder, "public.pem"), rsa.ExportRSAPublicKeyPem());
+
+Console.WriteLine($"Keys written to {folder}");
